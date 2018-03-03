46731d5bc1cf9ad3963db91aa0408112965081f5
Kevin Rocard <krocard@google.com>
2018-03-02 19:41:31 -0800

    msm8916-common: Migrate to audio V4
    Test: regression test on the board
    Bug: 38184704
    Change-Id: Ife559125850277865451f92ea862e7ec47fc9da7
    Merged-In: Ife559125850277865451f92ea862e7ec47fc9da7
    Cherry-picked from master
    Signed-off-by: Kevin Rocard <krocard@google.com>

--- device.mk
+++ device.mk
@@ -55,13 +55,13 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.0-service \
+    android.hardware.soundtrigger@2.0-service
 
 # Audio configuration file
 PRODUCT_COPY_FILES += \
