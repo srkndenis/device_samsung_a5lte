90a67bb4f464604f07bc97e233abe7fa62b886b0
srkndenis <srkndenis@gmail.com>
2020-02-10 18:41:09 +0000

    a5lte: add camera from serrano

--- device.mk
+++ device.mk
@@ -111,16 +111,15 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
-    camera.device@1.0-impl-legacy \
-    libcamera_shim \
+    Camera2 \
     libmm-qcamera \
     camera.msm8916 \
-    Snap
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl
 
-# Camera
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true
 
 # Doze
 PRODUCT_PACKAGES += \
