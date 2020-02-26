afba82e15d655f6eb536a2d8f41d444438ad5837
srkndenis <srkndenis@gmail.com>
2020-02-26 04:53:23 +0000

    a5lte: add back kemaster firmware

--- Android.mk
+++ Android.mk
@@ -45,6 +45,18 @@ $(ISDB_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(ISDB_SYMLINKS)
 
+KM_IMAGES := \
+    keymaste.b00 keymaste.b01 keymaste.b02 keymaste.b03 keymaste.mdt
+
+KM_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(KM_IMAGES)))
+$(KM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+    @echo "Keymaster firmware link: $@"
+    @mkdir -p $(dir $@)
+    @rm -rf $@
+    $(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(KM_SYMLINKS)
+
 # MCpay
 MCP_IMAGES := \
 	mcpay.b00 mcpay.b01 mcpay.b02 mcpay.b03 mcpay.mdt
