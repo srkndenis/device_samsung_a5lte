62077c9125c93f3d07b22bae269cc73c10fd7647
srkndenis <srkndenis@gmail.com>
2020-02-26 05:10:23 +0000

    a5lte: Remove redundant perfd prop

--- system_prop.mk
+++ system_prop.mk
@@ -117,7 +117,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.max_starting_bg=8 \
-    ro.vendor.extension_library=libqti-perfd-client.so \
     ro.core_ctl_min_cpu=0 \
     ro.core_ctl_max_cpu=4 \
     ro.min_freq_0=200000
