5de8121827c1b05a25cfd44c4e483f3e69381ce8
srkndenis <srkndenis@gmail.com>
2020-03-27 18:59:51 +0000

    a5lte: Bliss-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,12 +15,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a5lte.mk \
-    $(LOCAL_DIR)/havoc_a5lte.mk \
-    $(LOCAL_DIR)/dot_a5lte.mk \
-    $(LOCAL_DIR)/bootleg_a5lte.mk
+    $(LOCAL_DIR)/bliss_a5lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a5lte-user \
-    lineage_a5lte-userdebug \
-    lineage_a5lte-eng
+    bliss_a5lte-user \
+    bliss_a5lte-userdebug \
+    bliss_a5lte-eng
