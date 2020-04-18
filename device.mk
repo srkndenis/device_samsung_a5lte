debc39bac56346e465ed7c24bab58ffea1600513
srkndenis <srkndenis@gmail.com>
2020-04-18 06:57:18 +0000

    a5lte: add bluetooth a2dp audio

--- device.mk
+++ device.mk
@@ -68,6 +68,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-service \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl \
+    android.hardware.bluetooth.a2dp@1.0-impl \
     android.hardware.soundtrigger@2.0-service
 
 # Audio configuration file
