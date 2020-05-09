81b3a3af8a5f7ed6a6240dc3bccd1bf3a69c45da
Paul Keith <javelinanddart@gmail.com>
2020-05-09 11:35:35 -0400

    a5lte: Skip EV_REL input devices in recovery
    * Fixes touch in recovery, as some of our sensors are
      picked up as input devices.
    
    Change-Id: I3fd932e6bc6c3071c97bfb7c7a5cee0736a2aa5c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -204,6 +204,7 @@ TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE := true
 # Recovery
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_cm
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_SKIP_EV_REL_INPUT := true
 
 # SELinux
 include device/qcom/sepolicy-legacy/sepolicy.mk
