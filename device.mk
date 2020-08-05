e9448e8e9dd60df1bc9a1708cdce44e6fc76ad23
srkndenis <srkndenis@gmail.com>
2020-08-05 06:06:12 +0000

    a5lte: Exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO

--- device.mk
+++ device.mk
@@ -18,9 +18,14 @@
 # Inherit from common
 $(call inherit-product, device/samsung/qcom-common/qcom-common.mk)
 
-# Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-PRODUCT_ENFORCE_RRO_TARGETS := framework-res
+# Overlays
+DEVICE_PACKAGE_OVERLAYS := \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # ANT+
 PRODUCT_PACKAGES += \
