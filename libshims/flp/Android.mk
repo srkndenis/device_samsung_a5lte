c57818fb3ba03d19ceb94f6d08c36842e2f37d65
srkndenis <srkndcenis@gmail.com>
2020-10-21 10:13:58 +0400

    a5lte: Convert all shims to blue print

--- libshims/flp/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := flp.c
-
-LOCAL_MODULE := libshims_flp
-LOCAL_MODULE_TAGS := optional
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
