81c1f02a84dcb715d195fc392f5ba6f99d763768
sorden1974 <srkndenis@gmail.com>
2020-10-27 18:02:08 +0000

    a5lte: Add missing libhidltransport

--- device.mk
+++ device.mk
@@ -376,6 +376,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0 \
+    libhidltransport \
+    libhidltransport.vendor \
     libhwbinder \
     libhwbinder.vendor
 
