44baba2bd28659e0db9efc17545e7a97bc63d777
sorden1974 <srkndenis@gmail.com>
2020-11-04 08:54:21 +0000

    a5lte: Enable Livedisplay

--- device.mk
+++ device.mk
@@ -262,8 +262,8 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-service
 
 # LiveDisplay
-#PRODUCT_PACKAGES += \
-#    vendor.lineage.livedisplay@2.0-service.samsung-qcom
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service.samsung-qcom
 
 # OMX
 PRODUCT_PACKAGES += \
