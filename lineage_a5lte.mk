14e48bc29d1ee6745ae2a8f9379dfcc9f590fa29
ghostrider-reborn <adithya.r02@outlook.com>
2020-11-24 09:48:42 +0530

    a5lte: Override build description in addition to fingerprint
     * Required for passsing safetynet in R

--- lineage_a5lte.mk
+++ lineage_a5lte.mk
@@ -39,12 +39,14 @@ TARGET_OTA_ASSERT_DEVICE := a5ulte,a5ultexx,a5lte,a5ltexx,a53gxx,a53g,a5ltedd,a5
 
 # Fingerprint
 BUILD_FINGERPRINT := samsung/a5ltexx/a5lte:6.0.1/MMB29M/A500FXXU1CPH2:user/release-keys
+BUILD_DESCRIPTION := a5ltexx-user 6.0.1 MMB29M A500FXXU1CPH2 release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=a5ltexx \
     TARGET_DEVICE="a5ltexx"
-    PRIVATE_BUILD_DESC="a5ltexx-user 6.0.1 MMB29M A500FXXU1CPH2 release-keys"
-
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)"
