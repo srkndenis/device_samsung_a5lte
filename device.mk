4deb6b7d838468bb34369a2d9685daff3f627d1f
sorden1974 <srkndenis@gmail.com>
2020-12-18 20:37:35 +0000

    Revert "a5lte: Import relevant Android GO configs"
    This reverts commit cea672247bfc2d190fc6cd1f07db162179d303e9.

--- device.mk
+++ device.mk
@@ -445,10 +445,5 @@ PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 # for gms modules.
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 
-# Use a profile based boot image for this device. Note that this is currently a
-# generic profile and not Android Go optimized.
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-
 # Call the proprietary setup
 $(call inherit-product, vendor/samsung/a5lte/a5lte-vendor.mk)
