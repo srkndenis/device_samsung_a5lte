5e008ea7eb644d069935c08a2bfbe33f0c55d718
sorden1974 <srkndenis@gmail.com>
2021-01-28 09:00:21 +0000

    a5lte: Switch to A5-common init script and adapt for R

--- init/init_a5lte.cpp
+++ init/init_a5lte.cpp
@@ -32,63 +32,11 @@
 #include <android-base/file.h>
 #include <android-base/properties.h>
 #include <android-base/logging.h>
-#include <android-base/strings.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
 
-#define SIMSLOT_FILE "/proc/simslot_count"
-#define SERIAL_NUMBER_FILE "/efs/FactoryApp/serial_no"
-#define BT_ADDR_FILE "/efs/bluetooth/bt_addr"
-
 using android::base::GetProperty;
-using android::base::ReadFileToString;
-using android::base::Trim;
-
-__attribute__ ((weak))
-void init_target_properties()
-{
-}
-
-void property_override(char const prop[], char const value[], bool add = true)
-{
-    auto pi = (prop_info *) __system_property_find(prop);
-
-    if (pi != nullptr) {
-        __system_property_update(pi, value, strlen(value));
-    } else if (add) {
-        __system_property_add(prop, strlen(prop), value, strlen(value));
-    }
-}
-
-static void init_alarm_boot_properties()
-{
-    char const *boot_reason_file = "/proc/sys/kernel/boot_reason";
-    std::string boot_reason;
-    std::string tmp = GetProperty("ro.boot.alarmboot","");
-
-    if (ReadFileToString(boot_reason_file, &boot_reason)) {
-        /*
-         * Setup ro.alarm_boot value to true when it is RTC triggered boot up
-         * For existing PMIC chips, the following mapping applies
-         * for the value of boot_reason:
-         *
-         * 0 -> unknown
-         * 1 -> hard reset
-         * 2 -> sudden momentary power loss (SMPL)
-         * 3 -> real time clock (RTC)
-         * 4 -> DC charger inserted
-         * 5 -> USB charger insertd
-         * 6 -> PON1 pin toggled (for secondary PMICs)
-         * 7 -> CBLPWR_N pin toggled (for external power supply)
-         * 8 -> KPDPWR_N pin toggled (power key pressed)
-         */
-        if (Trim(boot_reason) == "3" || tmp == "true")
-            property_override("ro.alarm_boot", "true");
-        else
-            property_override("ro.alarm_boot", "false");
-    }
-}
 
 void property_override(const std::string& name, const std::string& value)
 {
@@ -119,29 +67,11 @@ void init_dsds() {
     property_override("persist.radio.multisim.config", "dsds");
 }
 
-void set_target_properties(const char *device, const char *model)
-{
-	char const *serial_number_file = SERIAL_NUMBER_FILE;
-	std::string serial_number;
-
-	char const *bt_addr_file = BT_ADDR_FILE;
-	std::string bt_address;
-
-	if (ReadFileToString(serial_number_file, &serial_number)) {
-		serial_number = Trim(serial_number);
-		property_override("ro.serialno", serial_number.c_str());
-	}
-
-	if (ReadFileToString(bt_addr_file, &bt_address)) {
-		bt_address = Trim(bt_address);
-		property_override("persist.service.bdroid.bdaddr", bt_address.c_str());
-        property_override("ro.boot.btmacaddr", bt_address.c_str());
-	}
-
-}
-
 void vendor_load_properties()
 {
+    // Init a dummy BT MAC address, will be overwritten later
+    property_override("ro.boot.btmacaddr", "00:00:00:00:00:00");
+
     std::string bootloader = GetProperty("ro.bootloader","");
 
     if (bootloader.find("A500FU") == 0) {
@@ -180,7 +110,7 @@ void vendor_load_properties()
         property_override("ro.build.description", "a5ultelgt-user 6.0.1 MMB29M A500LKLU1CQC1 release-keys");
         property_override_triple("ro.product.model", "ro.product.system.model", "ro.product.vendor.model", "SM-A500L");
         property_override_triple("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "a5ulte");
-		
+
     } else if (bootloader.find("A500H") == 0) {
         /* SM-A500H */
         property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a53gxx/a53g:6.0.1/MMB29M/A500HXXS1CQC5:user/release-keys");
@@ -195,35 +125,35 @@ void vendor_load_properties()
         property_override("ro.build.description", "a5ultedv-user 6.0.1 MMB29M A500YDVS1CRJ1 release-keys");
         property_override_triple("ro.product.model", "ro.product.system.model", "ro.product.vendor.model", "SM-A500Y");
         property_override_triple("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "a5ulte");
-		
+
     } else if (bootloader.find("A500F1") == 0) {
         /* SM-A500F1 */
         property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ultekx/a5ulteskt:6.0.1/MMB29M/A500F1U1CQB1:user/release-keys");
         property_override("ro.build.description", "a5ultekx-user 6.0.1 MMB29M A500F1U1CQB1 release-keys");
         property_override_triple("ro.product.model", "ro.product.system.model", "ro.product.vendor.model", "SM-A500F1");
         property_override_triple("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "a5ulteskt");
-		
+
     } else if (bootloader.find("A500S") == 0) {
         /* SM-A500S */
         property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ulteskt/a5ulteskt:6.0.1/MMB29M/A500SKSU1CQB1:user/release-keys");
         property_override("ro.build.description", "a5ulteskt-user 6.0.1 MMB29M A500SKSU1CQB1 release-keys");
         property_override_triple("ro.product.model", "ro.product.system.model", "ro.product.vendor.model", "SM-A500S");
         property_override_triple("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "a5ulteskt");
-		
+
     } else if (bootloader.find("A500K") == 0) {
         /* SM-A500K */
         property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ultektt/a5ultektt:6.0.1/MMB29M/A500KKTU1CQB1:user/release-keys");
         property_override("ro.build.description", "a5ultektt-user 6.0.1 MMB29M A500KKTU1CQB1 release-keys");
         property_override_triple("ro.product.model", "ro.product.system.model", "ro.product.vendor.model", "SM-A500K");
         property_override_triple("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "a5ultektt");
-		
+
     } else if (bootloader.find("A500W") == 0) {
         /* SM-A500W */
         property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ultebmc/a5ultebmc:6.0.1/MMB29M/A500WVLS1BPK2:user/release-keys");
         property_override("ro.build.description", "a5ultebmc-user 6.0.1 MMB29M A500WVLS1BPK2 release-keys");
         property_override_triple("ro.product.model", "ro.product.system.model", "ro.product.vendor.model", "SM-A500W");
         property_override_triple("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "a5ultebmc");
-		
+
     } else if (bootloader.find("A500YZ") == 0) {
         /* SM-A500YZ */
         property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ltezt/a5ltezt:5.0.2/LRX22G/A500YZZTU1BOK2:user/release-keys");
@@ -232,11 +162,6 @@ void vendor_load_properties()
         property_override_triple("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "a5ltezt");
     }
 
-    // Init a dummy BT MAC address, will be overwritten later
-    property_override("ro.boot.btmacaddr", "00:00:00:00:00:00");
-    init_target_properties();
-    init_alarm_boot_properties();
-	
     std::string device = GetProperty("ro.product.device", "");
     LOG(ERROR) << "Found bootloader id %s setting build properties for %s device\n" << bootloader.c_str() << device.c_str();
 }
